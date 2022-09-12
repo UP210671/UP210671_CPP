4fa5ef4bfcee8ea8271a4fd79607dc8c270340f2
UP210170 <up210170@alumnos.upa.edu.mx>
2022-09-12 10:50:24 -0500

    Nuevo U1

---
+++ U1/01_HolaMundo.cpp
@@ -0,0 +1,20 @@
+/*
+Date:12/09/2022
+Author: Cesar Eduardo Juarez Jasso
+Email: UP210671@alumnos.upa.edu.mx
+Descriptions:My first program in C++
+*/
+
+//--------------------PROCESSOR DIRECTIVES--------------------
+#include <iostream>
+//USAMOS IOSTREAM POR QUE SE IGNIFICA INPUT/OUTPUT
+//Input/output library between the screen "n" keybord
+
+//--------------------MAIN FUNCTION----------------------------
+    int main(){
+        //Print on screen a text
+        std::cout<<"Hellou World! This is my first program";
+
+    //Return 0 means that the programm ends succesfully
+    return 0;
+}
