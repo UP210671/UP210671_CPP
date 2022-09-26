9ffb11b5007f3392e92c9698b301cdbc2b4c64cf
UP210671 <up210671@alumnos.upa.edu.mx>
2022-09-26 10:37:17 -0500

    26/sep

---
+++ U2/Ejercicio2.cpp
@@ -0,0 +1,30 @@
+/*
+Date:12/09/2022
+Author: Cesar Eduardo Juarez Jasso
+Email: UP210671@alumnos.upa.edu.mx
+Descriptions:Exercise 2 
+*/
+#include <iostream>
+using namespace std;
+int main(){
+    float c;
+    cout<<"¿What is your calification?"<<endl;
+    cin>>c;
+    if ((c == 0)||(c == 0.4)||(c >= 0.6 && c <= 1)){
+        if (c ==0 ){
+            cout << "----------Unacceptable----------"<<endl;
+        }
+        else if (c == 0.4){
+            cout << "----------Acceptable----------"<<endl;
+        }
+        else if (c >= 0.6 && c <= 1){
+            cout << "----------Meritorious----------"<<endl;
+        }
+        cout << "Your bonus is: $" << 2400*c<<endl;
+        }
+        else {
+            cout << "----------Invalid rating----------"<<endl;
+
+        }
+        return 0;
+}
