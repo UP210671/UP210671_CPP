f16340a68046f935281114f63c912ea3c6fe9160
UP210671 <up210671@alumnos.upa.edu.mx>
2022-10-05 10:45:59 -0500

    05/oct

---
+++ U2/Ejercicio6.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int numproducto = 1;
+    float price, amount, total=0;
+    cout<<"Welcome to P.A.Y, plase input the amount and price of your products. To end program, type a 0\n\n";
+    do
+    {
+        /* code */
+        cout<<"Product"<<numproducto<<"\nAmount";
+        cin>>amount;
+        if (amount<=0)
+        {
+            /* code */
+            cout<<"\n----------Invalid amount,exiting program----------\n";
+            break;
+        }
+        cout<<"Price: ";
+        cin>>price;
+        if (price<0)
+        {
+            /* code */
+            cout<<"\n----------Invalid amount, exiting program----------\n";
+            break;
+        }
+        numproducto++;
+        total += (price * amount);
+    } while (price !=0);
+    cout<<"\nYou've inputted a total of "<<numproducto-2 << "different products. ";
+    cout<<"\nYour total is $"<<total<<" \n\n";        
+}
