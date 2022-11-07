84a9b1bb8b1e0a1e342fd97758e53f9c0e8417fe
UP210671 <up210671@alumnos.upa.edu.mx>
2022-11-07 09:25:44 -0600

    7nov

--- U3/04_Tic_Tac_Toe.cpp
+++ U3/04_Tic_Tac_Toe.cpp
@@ -1,210 +1,172 @@
-#include<iostream>
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <ctime>
+
 using namespace std;
- 
-char gato[3][3];
-bool bandera=true;
-//===PROTOTIPOS===
-void imprimegato();
-void cambiaturno(char &t);
-void introduce(char t, char corden);
-short evalua(char t);
-main()
-{
-imprimegato();
-char t='X';
-char posicion;
- 
- while(bandera==true)
- {
-   cout<<"Es el turno de "<<t<<" introduzca posicion: ";
-   cin>>posicion;
-   posicion=toupper(posicion);
-   introduce(t,posicion);
- 
-  // comprueba(turno);
-evalua(t);
-cambiaturno(t);
-   imprimegato();
-  }
- 
-system("pause");
-}//fin main
-//====INTRODUCE=====
-void introduce(char t, char corden)
-{
- switch(corden)
- {
-   case 'A':
-     if(gato[0][0]!='X' && gato[0][0]!='O')
-     {
-       gato[0][0]=t;
-//       cambiaturno(t);
-     }
-     else{cout<<"\n=Posicion ocupada=\n";}
-     break;
-   case 'B':
-     if(gato[0][1]!='X' && gato[0][1]!='O')
-     {
-       gato[0][1]=t;
-//       cambiaturno(t);
-     }
-     else{cout<<"\n=Posicion ocupada=\n";}
-     break;
- case 'C':
-     if(gato[0][2]!='X' && gato[0][2]!='O')
-     {
-       gato[0][2]=t;
-//       cambiaturno(t);
-     }
-     else{cout<<"\n=Posicion ocupada=\n";}
-     break;
-   case 'D':
-     if(gato[1][0]!='X' && gato[1][0]!='O')
-     {
-       gato[1][0]=t;
-//       cambiaturno(t);
-     }
-     else{cout<<"\n=Posicion ocupada=\n";}
-     break;
-   case 'E':
-     if(gato[1][1]!='X' && gato[1][1]!='O')
-     {
-       gato[1][1]=t;
- //      cambiaturno(t);
-     }
-     else{cout<<"\n=Posicion ocupada=\n";}
-     break;
-   case 'F':
-     if(gato[1][2]!='X' && gato[1][2]!='O')
-     {
-       gato[1][2]=t;
-//       cambiaturno(t);
-     }
-     else{cout<<"\n=Posicion ocupada=\n";}
-     break;
- case 'G':
-     if(gato[2][0]!='X' && gato[2][0]!='O')
-     {
-       gato[2][0]=t;
-//       cambiaturno(t);
-     }
-     else{cout<<"\n=Posicion ocupada=\n";}
-     break;
-   case 'H':
-     if(gato[2][1]!='X' && gato[2][1]!='O')
-     {
-       gato[2][1]=t;
-//       cambiaturno(t);
-     }
-     else{cout<<"\n=Posicion ocupada=\n";}
-     break;
-   case 'I':
-     if(gato[2][2]!='X' && gato[2][2]!='O')
-     {
-       gato[2][2]=t;
-//       cambiaturno(t);
-     }
-     else{cout<<"\n=Posicion ocupada=\n";}
-     break;
-   default:
-      cout<<"\nIntroduzca una posicion valida.\n";
- }//fin switch
-}//fin introduce
-//====CAMBIATURNO===
-void cambiaturno(char &t)
-{
-if (t=='X'){t='O';}
-else{t='X';}
-//cout<<t
-//return t;
-}//fin cambiaturno
- 
-//=======IMPRIMEGATO=====
-void imprimegato()
-{
-cout<<"\n     ######GATO#######\n\n";
- 
-cout<<"\t"<<gato[0][0]<<" | "<<gato[0][1]<<" | "<<gato[0][2]<<"        a | b | c"<<endl
-cout<<"\t----------"<<"      -----------\n";
-cout<<"\t"<<gato[1][0]<<" | "<<gato[1][1]<<" | "<<gato[1][2]<<"        d | e | f"<<endl
-cout<<"\t----------"<<"      -----------\n";
-cout<<"\t"<<gato[2][0]<<" | "<<gato[2][1]<<" | "<<gato[2][2]<<"        g | h | i"<<endl
- 
-cout<<"\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
- 
-}//fin imrpimegato
-short evalua(char t)
-{
-cout<<t
-short px=0;
-short po=0;
-short evaluacion=0;
- 
-cout<<"\n[0][i]\n";
-for(short i=0;i<3i++)
-   {
-   if(((gato[0][i]=='X')||gato[0][i]=='\0')&&((gato[1][i]=='X')||gato[1][i]=='\0')&&((gato[2][i]=='X')||gato[2][i]=='\0'))
-      {px++;}
-if(gato[0][0]=='O'||gato[0][1]=='O'||gato[0][2]=='O'||gato[1][0]=='O'||gato[1][1]=='O'||gato[1][2]=='O'||gato[2][0]=='O'||gato[2][1]=='O'||gato[2][2]=='O')
- {
-   if(((gato[0][i]=='O')||gato[0][i]=='\0')&&((gato[1][i]=='O')||gato[1][i]=='\0')&&((gato[2][i]=='O')||gato[2][i]=='\0'))
-      {po++;}
-  }
-      //else {po++;}
-cout<<"\npo="<<po
-cout<<"\npx="<<px
- 
-   }
- 
-for(short i=0;i<3i++)
-   {
- if(((gato[i][0]=='X')||gato[i][0]=='\0')&&((gato[i][1]=='X')||gato[i][1]=='\0')&&((gato[i][2]=='X')||gato[i][2]=='\0'))
-      {px++;}
-if(gato[0][0]=='O'||gato[0][1]=='O'||gato[0][2]=='O'||gato[1][0]=='O'||gato[1][1]=='O'||gato[1][2]=='O'||gato[2][0]=='O'||gato[2][1]=='O'||gato[2][2]=='O')
- {
- if(((gato[i][0]=='O')||gato[i][0]=='\0')&&((gato[i][1]=='O')||gato[i][1]=='\0')&&((gato[i][2]=='O')||gato[i][2]=='\0'))
-      {po++;}
-  }
- 
-//else{po++;}
- 
-cout<<"\npo="<<po
-cout<<"\npx="<<px
-    }//fin for      
- 
-//Diagonales
-cout<<"\ndiagonales\n";
- if(((gato[0][0]=='X')||gato[0][0]=='\0')&&((gato[1][1]=='X')||gato[1][1]=='\0')&&((gato[2][2]=='X')||gato[2][2]=='\0'))
-     {px++;}
-if(gato[0][0]=='O'||gato[0][1]=='O'||gato[0][2]=='O'||gato[1][0]=='O'||gato[1][1]=='O'||gato[1][2]=='O'||gato[2][0]=='O'||gato[2][1]=='O'||gato[2][2]=='O')
-{
- if(((gato[0][0]=='O')||gato[0][0]=='\0')&&((gato[1][1]=='O')||gato[1][1]=='\0')&&((gato[2][2]=='O')||gato[2][2]=='\0'))
-     {po++;}
- }
- 
-      //else{po++;}cout<<"\n1.diagonal\n";
-cout<<"\npo="<<po
-cout<<"\npx="<<px
- 
- 
-  if(((gato[2][0]=='X')||gato[2][0]=='\0')&&((gato[1][1]=='X')||gato[1][1]=='\0')&&((gato[0][2]=='X')||gato[0][2]=='\0'))
-     {px++;}
- 
-if(gato[0][0]=='O'||gato[0][1]=='O'||gato[0][2]=='O'||gato[1][0]=='O'||gato[1][1]=='O'||gato[1][2]=='O'||gato[2][0]=='O'||gato[2][1]=='O'||gato[2][2]=='O')
- {
-  if(((gato[2][0]=='O')||gato[2][0]=='\0')&&((gato[1][1]=='O')||gato[1][1]=='\0')&&((gato[0][2]=='O')||gato[0][2]=='\0'))
-     {po++;}
-  }
-    //else{po++;}cout<<"\n2.diagonal\n";
-cout<<"\npo="<<po
-cout<<"\npx="<<px
- 
- 
-if (t=='X'){evaluacion=px-po;}
-else{evaluacion=po-px;}
-cout<<"\n\nfinales";
-cout<<"\npo="<<po
-cout<<"\npx="<<px
-//return evaluacion;
-cout<<"\n\nEvaluacion es: "<<evaluacion<<endl
-}//fin evalua
+
+#define compareBoxes(box1, box2, box3) ((board[box1] == board[box2]) && (board[box2] == board[box3]) && (board[box1] != 0)) //Checkes if three items are the same, and makes sure they're not 0's.
+#define numberToLetter(x) ((x > 0) ? (x == 1) ? 'X' : 'O' : ' ') //Takes the number and turns it into the letter or space.
+
+int getWinner(int board[9]) {
+	//Finds winner of game, if there is no winner, returns 0.
+	int winner = 0;
+	for (int x = 0; x < 3; x++) {
+		if (compareBoxes(3*x, 3*x+1, 3*x+2)) { //Chekcs rows.
+			winner = board[3*x];
+			break;
+		} else if (compareBoxes(x, x+3, x+6)) { //Checks columns.
+			winner = board[x];
+			break;
+
+		} else if (compareBoxes(2*x, 4, 8-2*x) && (x < 2)) { //Checks diagonals. Doesn't check if x == 2.
+			winner = board[4];
+			break;
+		}
+	}
+	return winner;
+}
+bool gameOver(int board[9]){
+	//Checks if game is over, and announces who won, or if it was a tie.
+	int winner = getWinner(board);
+	if (winner > 0) {
+		cout << numberToLetter(winner) << " wins!"<< endl;
+		return true;
+	} 
+	for (int x = 0; x < 9; x++) {
+		if (board[x] == 0) return false;
+	}
+	cout << "Tie!\n\n";
+	return true;
+}
+
+int willWin(int board[9], int player) {
+	//Checks if a given player could win in the next plank.
+	for (int x = 0; x < 9; x++) {
+		int tempBoard[9];
+		memcpy(tempBoard, board, 36);
+		if (board[x] > 0) continue;
+		tempBoard[x] = player;
+		if(getWinner(tempBoard) == player) return x;
+	}
+	return -1;
+}
+
+int exceptionalCase(int board[9]) {
+	//Finds bords that are exceptions to how the algorithm works.
+	int cases[2][9] = {{1,0,0,0,2,0,0,0,1}, {0,1,0,1,2,0,0,0,0}}; //Boards that don't work with algorithm.
+	int answers[2][4] = {{3,3,3,3}, {2,8,6,0}};
+	int rotatedBoard[9] = {6,3,0,7,4,1,8,5,2};
+	int newBoard[9];
+	int tempBoard[9];
+	for(int x = 0; x < 9; x++) {
+		newBoard[x] = board[x];
+	}
+	for (int caseIndex = 0; caseIndex < 2; caseIndex++) {
+		for(int rotation = 0; rotation < 4; rotation++) {
+			for (int x = 0; x < 9; x++) 
+				tempBoard[x] = newBoard[x];
+			
+			int match = 0;
+			//Rotates board so it works with different versions of the same board.
+			for (int box = 0; box < 9; box++) {
+				newBoard[box] = tempBoard[rotatedBoard[box]];
+			}
+
+			for (int x = 0; x < 9; x++) {
+				if (newBoard[x] == cases[caseIndex][x]) match++;
+				else break;
+			}
+			if (match == 9) return answers[caseIndex][rotation];
+		}
+	}
+	return -1;
+}
+
+int getSpace(int board[9], int spaces[4]) {
+	//Gets a random corner or side that's not taken.
+	bool isSpaceEmpty = false;
+	int y;
+	for (int x = 0; x < 4; x++) {
+		if (board[spaces[x]] == 0) {
+			isSpaceEmpty = true;
+			break;
+		}
+	}
+	if (isSpaceEmpty) {
+		do {
+			y = rand() % 4;
+		} while (board[spaces[y]] != 0);
+		return spaces[y];
+	}
+	return -1;
+}
+
+void outputBoard(int board[9]) {
+	for(int line = 0; line < 3; line++){
+		for (int box = 0; box < 3; box++) {
+			cout << numberToLetter(board[3*line+box]) << ((box < 2) ? '|' : '\n');
+		}
+		cout << ((line < 2) ? "-----\n" : "\n");
+	}
+}
+
+int main(){
+	int board[9] = {0,0,0,0,0,0,0,0,0}; //Starts empty board.
+	int possibleWinner;
+	int move;
+	bool isInvalid;
+	string moveString;
+	srand((int) time(0));
+	int corners[4] = {0,2,6,8};
+	int sides[4] = {1,3,5,7};
+
+	cout << "1|2|3\n-----\n4|5|6\n-----\n7|8|9\n\n";
+
+	while (true) {
+		//Player X decides what move they'll do.
+		do {
+			cout << "X: ";
+			getline(cin, moveString);
+			move = moveString[0] - '1';
+			if (move > 8 || move < 0 || board[move] != 0) {
+				cout << "Invalid input" << endl;
+				isInvalid = true;
+			} else {
+				board[move] = 1;
+				isInvalid = false;
+				cout << endl;
+			}
+		} while (isInvalid);
+
+		//Decides whether or not the game continues.
+		if (gameOver(board) > 0) {
+			outputBoard(board);
+			break;
+		}
+
+		//Player O decides which move they'll do.
+		bool good = false;
+		for (int x = 2; x > 0; x--){
+			possibleWinner = willWin(board, x);
+			if (possibleWinner != -1) {
+				board[possibleWinner] = 2;
+				good = true;
+				break;
+			}
+		}
+		if (good);
+		else if (board[4] == 0) board[4] = 2; //Middle.
+		else if (exceptionalCase(board) > -1) board[exceptionalCase(board)] = 2; //Exception boards.
+		else if (getSpace(board, corners) != -1) board[getSpace(board, corners)] = 2; //Corners
+		else board[getSpace(board, sides)] = 2; //Sides
+		
+		//Prints the board to the screen.
+		outputBoard(board);
+
+		//Decides whether or not the game continues.
+		if(gameOver(board)) break;
+
+	}
+	return 0;
+}
